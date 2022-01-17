de42aca824598b779bd570a30863d8b797918b3c
Veronika <veronika.lopatiuk@gmail.com>
2022-01-17 19:16:52 +0000

    add delete functionality

--- cyf-albums-client/src/App.js
+++ cyf-albums-client/src/App.js
@@ -1,16 +1,17 @@
 import { useContext, useEffect, useState } from "react";
-import { Anchor, Header, Nav, Main, Box, Card, Grid, ResponsiveContext, Text, Footer } from 'grommet';
+import { Anchor, Header, Nav, Main, Box, Card, Grid, ResponsiveContext, Text, Footer, Button } from 'grommet';
 import "./style.css"
 
 function App() {
   const [cards, setCards] = useState([]);
-
   const size = useContext(ResponsiveContext);
 
+  const getAlbums=() => fetch("http://localhost:4000/albums")
+                    .then(res => res.json())
+                    .then(data => setCards(data))
+
   useEffect(() => {
-    fetch("http://localhost:4000/albums")
-      .then(res => res.json())
-      .then(data => setCards(data))
+    getAlbums();
   }, [])
 
   return (
@@ -30,6 +31,22 @@ function App() {
                 <p>Artist: {card.artistName}</p>
                 <p>Album: {card.collectionName}</p>
                 <a href={card.url}>YouTube Link</a>
+                <Box align="center" pad="medium">
+                  <Button label="Delete" onClick={() => {
+                    fetch("http://localhost:4000/albums/" + card.albumId, {
+                      method: "DELETE"
+                    })
+                    .then(res => {
+                      if(res.status <= 299 && res.status >= 200){
+                        getAlbums();
+                      }
+                    })
+                      
+                  }}/>
+                </Box>
+                <Box align="center" pad="medium">
+                  <Button label="Edit" onClick={() => {}}/>
+                </Box>
               </Card>
             );
           })}
