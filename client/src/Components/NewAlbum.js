61a72e8d9585da7aab030c233c6142b3bc120075
Veronika <veronika.lopatiuk@gmail.com>
2022-01-28 19:55:38 +0000

    add genre to newAlbum

--- client/src/Components/NewAlbum.js
+++ client/src/Components/NewAlbum.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Box, Button, Text, Layer, TextInput, DateInput } from "grommet"
+import { useState, useEffect } from "react";
+import { Box, Button, CardHeader, Layer, Card, Select, Text, TextInput, DateInput } from "grommet"
 import { FormClose } from "grommet-icons";
 
 const NewAlbum = ({onClose, getAlbums}) => {
@@ -7,6 +7,16 @@ const NewAlbum = ({onClose, getAlbums}) => {
     const [artist, setArtist] = useState("");
     const [album, setAlbum] = useState("");
     const [url, setUrl] = useState("");
+    const [genre, setGenre] = useState("");
+    const [options, setOptions] = useState([]);
+
+    const getGenre = () => fetch("http://localhost:4000/genre")
+                        .then(res => res.json())
+                        .then(data => setOptions(data));
+
+    useEffect(() => {
+        getGenre();
+        }, [])
 
     const onChange = (event) => {
         const nextDate = event.value;
@@ -20,7 +30,8 @@ const NewAlbum = ({onClose, getAlbums}) => {
                 artistName: artist,
                 collectionName: album,
                 releaseDate: date,
-                url: url
+                url: url,
+                genreId: options.find( ({ name }) => name === genre ).id
             }),
             headers: {
                 "Content-type": "application/json"
@@ -41,16 +52,16 @@ const NewAlbum = ({onClose, getAlbums}) => {
             onClickOutside={onClose}
             onEsc={onClose}
         >
-        <Box
+        <Card
             pad="medium"
             gap="small"
-            width={{ min: 'medium' }}
+            width={{ min: 'large' }}
             height={{ min: 'small' }}
             fill
         >
             <Button alignSelf="end" icon={<FormClose />} onClick={onClose} />
-            <Text>Add New Album</Text>
-            <Box width="medium">
+            <CardHeader alignSelf="center">Add New Album</CardHeader>
+            <Box width="large">
                 <Box>
                     Artist:<TextInput value={artist} onChange={(e) => setArtist(e.target.value)} />
                 </Box>
@@ -58,22 +69,31 @@ const NewAlbum = ({onClose, getAlbums}) => {
                     Album:<TextInput value={album} onChange={(e) => setAlbum(e.target.value)} />
                 </Box>
                 <Box>
+                    <Text>Genre:</Text>
+                    <Select
+                        placeholder="Clear Selection"
+                        value={genre}
+                        options={options.map(genre => genre.name)}
+                        onChange={(e) => {
+                            setGenre(e.target.value);
+                        }}
+                    />
+                </Box>
+                <Box>
                     Url:<TextInput value={url} onChange={(e) => setUrl(e.target.value)} />
                 </Box>
                 <Box>
                     Release Date:<DateInput format="dd/mm/yyyy" value={date} onChange={onChange} />
                 </Box>
-                <Box align="center" pad="medium">
-                <Button label="Save" onClick={() => {
-                    post();
-                    onClose();
-                }}/>
-                </Box>
-                <Box align="center" pad="medium">
-                    <Button label="Cancel" onClick={onClose}/>
+                <Box alignSelf="center" pad="medium" direction="row">
+                    <Button label="Save" margin={{right: "10px"}} onClick={() => {
+                        post();
+                        onClose();
+                    }}/>
+                    <Button label="Cancel" margin={{left: "10px"}} onClick={onClose}/>
                 </Box>
             </Box>
-        </Box>
+        </Card>
         </Layer>
     );
 }
