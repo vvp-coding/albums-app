6e34e845ca3a7b3aa08edbf66958629ef246ab26
Veronika <veronika.lopatiuk@gmail.com>
2022-01-30 14:26:52 +0000

    refactor code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import { useEffect, useState, useRef } from "react";
-import { Header, Nav, Main, Box, Grid, Text, Footer, Button, TextInput, Layer } from 'grommet';
-import { Search } from "grommet-icons"
+import { Header, Nav, Main, Box, Grid, Text, Footer, Button} from 'grommet';
 import "./style.css"
 import AlbumCard from "./Components/AlbumCard";
 import NewAlbum from "./Components/NewAlbum";
 import VideoPlayer from "./Components/VideoPlayer";
+import SearchAlbums from "./Components/SearchAlbums";
 
 function App() {
   const [cards, setCards] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [openAlbum, setOpenAlbum] = useState(false);
   const [searching, setSearching] = useState(false);
   const [watchVideo, setWatchVideo] = useState(false);
   const videoLink = useRef(null);
 
-  const onClose = () => setOpen(undefined);
+  const onClose = () => setOpenAlbum(false);
 
-  const stopSearching = () => setSearching(undefined);
+  const stopSearching = () => setSearching(false);
 
   const toggleWatch = (isWatching, url) => {
     videoLink.current = url;
@@ -26,7 +26,7 @@ function App() {
                     .then(res => res.json())
                     .then(data => setCards(data))
 
-  const search = (searchData) => fetch("http://localhost:4000/search?search=" + searchData)
+  const searchAlbums = (text) => fetch("http://localhost:4000/search?search=" + text)
                                   .then(res => res.json())
                                   .then(data => setCards(data))
 
@@ -39,7 +39,7 @@ function App() {
       <Header background="light-4" pad="small">
         <Nav direction="row">
           <Box>
-            <Button label="Add Album" onClick={() => setOpen(true)} />
+            <Button label="Add Album" onClick={() => setOpenAlbum(true)} />
           </Box>
           <Box>
             <Button label="Search" onClick={() => {
@@ -59,25 +59,9 @@ function App() {
           })}
         </Grid>
       </Box>
-      { open && <NewAlbum onClose={onClose} getAlbums={getAlbums} /> }
+      { openAlbum && <NewAlbum onClose={onClose} getAlbums={getAlbums} /> }
       { watchVideo && <VideoPlayer url={videoLink.current} toggleWatch={toggleWatch} /> }
-      {searching && 
-          <Layer
-            full={false}
-            position="top"
-            onClickOutside={stopSearching}
-            onEsc={stopSearching}
-          >
-            <Box
-              pad="medium"
-              gap="small"
-              width={{ min: 'medium' }}
-              height={{ min: 'small' }}
-              fill
-            >
-              <TextInput icon={<Search />} reverse placeholder="Search..." onChange={(e) => search(e.target.value)} />
-            </Box>
-          </Layer>}
+      {searching && <SearchAlbums stopSearching={stopSearching} searchAlbums={searchAlbums} />}
       </Main>
       <Footer background="light-4" justify="center" pad="small">
         <Text textAlign="center" size="small">
